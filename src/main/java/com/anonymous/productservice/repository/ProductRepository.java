ec8725dc32086b4c7b3a735865688deffcb427a5
seaZonal2001 <kausiksurjit@gmail.com>
2023-03-05 00:20:05 +0530

    created needful layers

---
+++ src/main/java/com/anonymous/productservice/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.anonymous.productservice.repository;
+
+import com.anonymous.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product,String> {
+}
